faf0871aeb0fd3f2ebeeefcf386c9d43f9f696af
cremacious <chrismackall3@gmail.com>
2025-07-16 12:40:02 -0400

    Fixed spacing

--- src/components/dashboard/QuickActions.tsx
+++ src/components/dashboard/QuickActions.tsx
@@ -4,19 +4,19 @@ import FindFriendDialog from '../social/FindFriendDialog';
 
 const QuickActions = () => {
   return (
-    <div className="bg-white rounded-lg p-4 hover:shadow-md transition-all">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+    <div className="bg-white rounded-lg p-3 hover:shadow-md transition-all">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
         <Link href={'/purchases/new'}>
           <div className="h-auto p-4 bg-gradient-to-r from-green-500 to-emerald-600 hover:from-green-600 hover:to-emerald-700 text-white flex flex-col items-center space-y-2 rounded-xl font-bold ">
             <CircleDollarSign size={40} />
-            <div className="text-lg">Log Purchase</div>
+            <div>Log Purchase</div>
           </div>
         </Link>
 
         <Link href={'/stash/new'}>
           <div className="h-auto p-4 bg-gradient-to-r from-yellow-500 to-orange-600 hover:from-yellow-600 hover:to-orange-700 text-white flex flex-col items-center space-y-2 rounded-xl">
             <Container size={40} />
-            <div className="text-lg">Add to Stash</div>
+            <div>Add to Stash</div>
           </div>
         </Link>
 
@@ -25,7 +25,7 @@ const QuickActions = () => {
             <span className="text-2xl">
               <Pen size={40} />
             </span>
-            <span className="font-medium">Create Post</span>
+            <div>Create Post</div>
           </div>
         </Link>
 
@@ -34,7 +34,7 @@ const QuickActions = () => {
             <span className="text-2xl">
               <SmilePlus size={40} />
             </span>
-            <span className="font-medium">Find Friends</span>
+            <span>Find Friends</span>
           </div>
         </FindFriendDialog>
       </div>
